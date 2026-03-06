00ea661048f28c48b4ea26e35608624089cdc64b
pfischill <pfischill@student.ethz.ch>
2026-03-06 21:29:12 +0100

    fix bug

--- src/Interpolation/Scatter/TiledScatter.h
+++ src/Interpolation/Scatter/TiledScatter.h
@@ -254,9 +254,8 @@ namespace ippl::Interpolation::detail {
             size_t n = 1;
             for (unsigned d = 0; d < Dim; ++d)
                 n *= static_cast<size_t>(tile_size[d] + W + 1);
-            const size_t scratch = (IsComplex ? 2 : 1) * n * sizeof(RealType);
-
-            return scratch;
+            // Use shmem_size() to account for Kokkos alignment overhead
+            return (IsComplex ? 2 : 1) * scratch_view::shmem_size(n);
         }
 
         void run(size_t) {
