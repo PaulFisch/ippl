49382d93b997db88f41aed0e4326beb083ae7a4a
pfischill <pfischill@student.ethz.ch>
2026-05-04 00:32:32 +0200

    fix nvcc errors

--- unit_tests/Interpolation/KernelGatherScatterTest.cpp
+++ unit_tests/Interpolation/KernelGatherScatterTest.cpp
@@ -618,6 +618,7 @@ public:
             "create_dense_particles", Kokkos::RangePolicy<ExecSpace>(0, nParticles),
             KOKKOS_LAMBDA(size_t i) {
                 typename RandPool::generator_type gen = randPool.get_state();
+                (void)extent_local; // Fix nvcc problems
 
                 ippl::Vector<T, Dim> pos;
                 pos[0] = centerX + (gen.drand() - 0.5) * spread;
